244a856338931f95ab9960fbb8068cb49045528b
FabrizioOtranto <Fabryotranto@gmail.com>
2022-08-16 21:54:57 -0300

    Clase 3

--- cypress/e2e/localizandoElementos.cy.js
+++ cypress/e2e/localizandoElementos.cy.js
@@ -1,6 +1,7 @@
-describe("Locanlizando elementos con css Selector", () =>{
+describe("Locanlizando elementos con css Selector", () => {
 
-    xit("Localizando elementos Utilizando cssSelectors", () =>{
+
+    xit("Localizando elementos Utilizando cssSelectors", () => {
         cy.visit("/");
         cy.get('button').should("exist");
         cy.get('[id="user"]').should('exist');
@@ -11,43 +12,64 @@ describe("Locanlizando elementos con css Selector", () =>{
         cy.get('[class="chakra-input password css-1ekf6i8"]')
     })
 
-    it('Localizando elementos utilizando find', () =>{
+    it('Localizando elementos utilizando find', () => {
         cy.visit('/')
         cy.get('fieldset').find("[value='Male']")
-    })  
+    })
 
-    it('Localizando elementos utilizando children', () =>{
+    it('Localizando elementos utilizando children', () => {
         cy.visit('/')
         cy.get('#year').children("[value='1995']")
-    }) 
+    })
 
-    it('Localizando elementos utilizando parent', () =>{
+    it('Localizando elementos utilizando parent', () => {
         cy.visit('/')
         cy.get('[value="1995"]').parent('#year')
-    }) 
+    })
 
-    it('Localizando elementos utilizando hermanos', () =>{
+    it('Localizando elementos utilizando hermanos', () => {
         cy.visit('/')
         cy.get('#user').siblings('label')
-    }) 
+    })
 
-    it('Localizando elementos utilizando contains', () =>{
+    it('Localizando elementos utilizando contains', () => {
         cy.visit('/')
         cy.get('button').contains('Register')
         cy.contains('Register')
-        cy.contains('register', {matchCase:false})
-    }) 
+        cy.contains('register', { matchCase: false })
+    })
 
-    it.skip("Utilizando variables dentro de selectores", () =>{
+    it.skip("Utilizando variables dentro de selectores", () => {
         let usuario = "pushingit"
         cy.get(`[id=user_${usuario}_3]`)
     })
 
-    it.only("ingresar un valor presionando enter", () =>{
+    it("ingresar un valor presionando enter", () => {
         cy.visit('/')
         cy.get("#user").type("usuario {enter}")
         cy.get("#user").clear()
         let user = 'user'
         cy.get("#user").type(`${user} {enter}`)
+    })
+
+    it.only("Buscar todas las tareas por clase y filtrar por nombre", () => {
+        var numero = Math.floor(Math.random() * 100)
+        cy.visit("https://pushing-front.vercel.app/")
+        cy.get('#user').type(`FrancoBartomioli${numero}`)
+        cy.get('#pass').type("123asd11!!")
+        cy.get('[value="Male"]').check({ force: true })
+        cy.get('#day').select(23)
+        cy.get('#month').select('November')
+        cy.get('#year').select('1995')
+        cy.get('#submitForm').click()
+        cy.get('#todolistlink').should('exist')
+        cy.get('#todolistlink').click()
+        cy.get('#task').type('Tarea 1')
+        cy.get('#sendTask').click()
+        cy.get('#task').type('Tarea 2')
+        cy.get('#sendTask').click()
+        cy.get('#task').type('Tarea 3')
+        cy.get('#sendTask').click()
+        cy.get("[class='chakra-text css-8atqhb']").contains("Tarea 1");
     })
 })
