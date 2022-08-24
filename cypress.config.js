6ba3d54cc5d6bb1187846bc953dce8721d7cf4f5
FabrizioOtranto <Fabryotranto@gmail.com>
2022-08-23 22:01:00 -0300

    clase-5

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 4000,
     watchForFileChanges: false,
     "baseUrl": "https://pushing-front.vercel.app/",
   },
