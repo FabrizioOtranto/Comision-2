ed33353b7acf9337921e453cff6fbe395059b5b9
FabrizioOtranto <Fabryotranto@gmail.com>
2022-08-25 21:30:36 -0300

    Clase-6

---
+++ cypress/support/pages/homePage.js
@@ -0,0 +1,9 @@
+export class HomePage {
+    constructor(){
+        this.todolistLink = "#todolistlink"
+    }
+
+    clickTodoListLink(){
+        cy.get(this.todolistLink).click();
+    }
+}
---
+++ cypress/support/pages/todoListPage.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+export class TodoListPage {
+    constructor(){
+        this.taskInput = "#task";
+        this.sendTaskButton = "//button[@id='sendTask']";
+    }
+
+    escribirTarea(tarea){
+        cy.get(this.taskInput).type(tarea)
+    }
+
+    clickSendButton(){
+        cy.xpath(this.sendTaskButton).click()
+    }
+
+    completarTarea(tarea){
+        cy.contains(tarea).click();
+    }
+
+    eliminarTarea(tarea){
+        cy.contains(tarea).siblings("button").click()
+    }
+}
